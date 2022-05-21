f845a8f747fe492feb4c7b97899434446adddd11
João Vitor Liberato de Oliveira <101505157+JoaoVitorLiberato@users.noreply.github.com>
2022-05-21 18:37:37 -0300

    Ajustando o Banco de Dados.

--- src/app.js
+++ src/app.js
@@ -1,6 +1,19 @@
 const express = require("express");
+const DataBase = require("./database/index")
+
 
 const app = express();
+app.use(express.json());
+
+DataBase.hasConnection();
+
+
+
+
+
+
+
+
 
 app.listen(3001, () => console.log("Running server at port: 3001."));
 
