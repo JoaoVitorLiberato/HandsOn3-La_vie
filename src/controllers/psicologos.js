76b279458e96b25705dd73f71dc8afe5f7f4f066
João Vitor Liberato de Oliveira <101505157+JoaoVitorLiberato@users.noreply.github.com>
2022-05-22 22:56:55 -0300

    Iniciando as routes e controllers da tabela psicologo.

---
+++ src/controllers/psicologos.js
@@ -0,0 +1,81 @@
+const { Psicologo } = require("../models");
+
+const PsicologoController = {
+
+    AllList: async (req, res) => {
+        try {
+
+            const allPsicologos = await Psicologo.findAll();
+            res.json(allPsicologos);
+            res.status(200).json({ menssage: "Success in listing all psychologists in La vie." });
+
+        } catch (error) {
+
+            console.error(error.message);
+            res.status(500).json({ erro: "Shiiiii, There were system problems!" });
+
+        }
+    },
+
+    CreateNewPsicologo: async (req, res) => {
+
+        try {
+
+            const { nome, email, senha, apresentacao } = req.body;
+
+            const newPsicologo = await Psicologo.create(
+                {
+                    nome,
+                    email,
+                    senha,
+                    apresentacao
+
+                },
+
+            );
+
+
+
+            res.json(newPsicologo);
+            res.status(201).json({ menssage: "New psychologist successfully created! " });
+
+
+
+
+        } catch (error) {
+
+            console.log(error.message);
+            res.status(500).json({ erro: "Shiiiii, There were system problems!" });
+
+        }
+
+
+
+    },
+
+    findByID: async (req, res) => {
+        const { id } = req.params;
+
+        try {
+            const SeachPsicologo = await Psicologo.findByPk(id);
+
+            if (SeachPsicologo) {  
+               res.status(200).json(SeachPsicologo);
+            }
+
+            res.status(404).json({ message : "Psychologist not found!" });
+            
+        } catch (error) {
+            
+            res.status(500).json({ error: "Shiiiii, There were system problems!" });
+        
+        }
+
+
+    },
+
+
+}
+
+
+module.exports = PsicologoController;
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,9 +3,9 @@ const  { route }  = require("express/lib/application");
 
 const routes = express.Router();
 
-const {home} = require("../controlles/home");
+const {home} = require("../controllers/home");
 
-const PsicologoController = require("../controlles/psicologos");
+const PsicologoController = require("../controllers/psicologos");
 
 
 
@@ -14,6 +14,8 @@ routes.get("/", home);
 
 
 routes.get("/psicologo/allList", PsicologoController.AllList);
+routes.post("/psicologo/CreateNewPsicologo", PsicologoController.CreateNewPsicologo);
+routes.get("/psicologo/findByID/:id", PsicologoController.findByID);
 
 
 
